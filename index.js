44cf6ff3a6e013b9e04458287ac2273075f2d055
ladelpriore <lucasdelpriore@gmail.com>
2024-10-14 14:24:34 -0500

    Commit block 7 workshop.

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+
+const codeONE = 5+5;  //Use the addition operator 
+const codeTWO = 20 * 2; //Use the multiplication operator
+const codeTHREE = 40 - 1; //Use the subtraction operator
+
+console.log("Create 3 variables with unique arithmetic operators.");
+
+alert(`You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${codeONE} - ${codeTWO} - ${codeTHREE}.`); 
+//Use ${..} to easily insert numbers into string 
